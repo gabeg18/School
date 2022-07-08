89034999c52baeb72bfdc0c9114f8f9106127a9c
Gabriel Gutierrez <gabe.gutierrez18@gmail.com>
2022-07-08 18:37:59 -0400

    Modified MAIN2.f90 to use allocatable arrays

--- MAIN2.f90
+++ MAIN2.f90
@@ -1,21 +1,44 @@
 PROGRAM MAIN
   
-  ! Program to test the use of the block_gen module
+  ! Program to test the use of the mblock_gen and block_data modules
 
   USE, INTRINSIC :: ISO_FORTRAN_ENV
-  USE block_gen
+  !USE block_data
+  USE mblock_gen
 
   IMPLICIT NONE
 
-  INTEGER :: iMin = 1,  &
-             iMax = 101
+  INTEGER, PARAMETER :: iMin       = 1,      & 
+                        iMax       = 101
+
+  INTEGER :: num_blocks
 
   REAL(KIND=rDef), PARAMETER :: x_min   =   0.0, &
                                 x_max   = 100.0, &
                                 delta_x =   1.0
 
-  REAL(KIND=rDef), DIMENSION(6) :: block_min, block_max
+  REAL(KIND=rDef), DIMENSION(:), ALLOCATABLE :: block_min, & 
+                                                block_max
 
-  CALL blocks(x_min,x_max,delta_x,block_min,block_max)
+
+  ! Data Input
+  PRINT *,"Please input the number of blocks you wish to use"
+  READ *,num_blocks
+
+  ALLOCATE( &
+    block_min(num_blocks), &
+    block_max(num_blocks))
+
+  ! Call Multi-Block Generator from mblock_gen Module
+  CALL blocks(x_min,      &
+              x_max,      &
+              delta_x,    &
+              num_blocks, &
+              block_min,  &
+              block_max)
+
+  DEALLOCATE( &
+    block_min,&
+    block_max)
 
 END PROGRAM MAIN
--- MultiBlockGen.f90
+++ MultiBlockGen.f90
@@ -1,4 +1,4 @@
-MODULE block_gen
+MODULE mblock_gen
   USE, INTRINSIC :: ISO_FORTRAN_ENV
   IMPLICIT NONE
 
@@ -16,21 +16,23 @@ MODULE block_gen
   SUBROUTINE blocks(x_min,      & ! Global Minimum (Computational Domain)
                     x_max,      & ! Global Maximum (Computational Domain)
                     delta_x,    & 
+                    num_blocks, & ! User-defined Number of Blocks
                     block_min,  & ! Individual Block Minimum
                     block_max)    ! Individual Block Maximum
 
     ! Dummy Variable Declaration
+    INTEGER, INTENT(IN) :: num_blocks
     REAL(KIND=rDef), INTENT(IN) :: x_min, x_max, delta_x
-    REAL(KIND=rDef), DIMENSION(6), INTENT(OUT) :: block_min, block_max
+    REAL(KIND=rDef), DIMENSION(num_blocks), INTENT(OUT) :: block_min, block_max
 
     ! Local Variable Declaration
-    INTEGER :: num_blocks, i
+    INTEGER :: i
 
-    num_blocks = 6                              ! Modify later for diff number of blocks?
+    !num_blocks = 6                              ! Modify later for diff number of blocks?
 
     ! Calculate Block Minimums and Maximums
     block_min(1) = x_min                        ! Set minimum for first block
-    block_max(1) = block_min(1) + x_max/6.0
+    block_max(1) = block_min(1) + REAL(x_max/num_blocks)
 
     WRITE(UNIT=6,FMT=201) 1,block_min(1),block_max(1)   ! Write results to screen
 
@@ -39,17 +41,17 @@ MODULE block_gen
     DO i = 2,num_blocks
 
       block_min(i) = x_min + block_max(i-1)
-      block_max(i) = block_min(i) + x_max/6.0           
+      block_max(i) = block_min(i) + REAL(x_max/num_blocks)           
       WRITE(UNIT=6,FMT=201) i,block_min(i),block_max(i) ! Write results to screen
 
     END DO
 
     
     ! Format Statement(s)
-    201 FORMAT("Block",I2,T12,"Lower Bound:",F8.3,T39,"Upper Bound:",F15.3)
+    201 FORMAT("Block",I3,T12,"Lower Bound:",F8.3,T39,"Upper Bound:",F15.3)
     
 
   END SUBROUTINE blocks
 
-END MODULE block_gen
+END MODULE mblock_gen
 
